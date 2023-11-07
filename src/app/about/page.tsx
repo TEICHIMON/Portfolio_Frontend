9bc16e270271f37651e65e3890887e8eff733830
sudami <sudami125@gmail.com>
2023-11-07 22:45:29 +0900

    feat(footer components): tailwindcss 导入失败bug

---
+++ src/app/about/page.tsx
@@ -0,0 +1,63 @@
+// import React from "react";
+// import Image from "next/image";
+// import Button from "@/components/Button/Button";
+//
+// const About = () => {
+//     return (
+//         <div className="container mx-auto px-4">
+//             <div className="w-full h-72 relative">
+//                 <Image
+//                     src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+//                     layout="fill"
+//                     objectFit="cover"
+//                     alt=""
+//                     className="object-cover filter grayscale"
+//                 />
+//                 <div className="absolute bottom-5 left-5 bg-[#53c28b] p-1.5 text-white space-y-2">
+//                     <h1 className="text-4xl font-bold">Digital Storytellers</h1>
+//                     <h2 className="text-xl font-medium">
+//                         Handcrafting award winning digital experiences
+//                     </h2>
+//                 </div>
+//             </div>
+//             <div className="flex space-x-24 mt-12">
+//                 <div className="flex-1 space-y-7.5">
+//                     <h1 className="text-3xl font-semibold">Who Are We?</h1>
+//                     <p className="text-lg font-light text-justify">
+//                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+//                         quae dolor, optio voluptatibus magnam iure esse tempora beatae. A
+//                         suscipit eos. Animi quibusdam cum omnis officiis voluptatum quo ea
+//                         eveniet? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+//                         Ducimus quae dolor, optio voluptatibus magnam iure esse tempora
+//                         beatae, a suscipit eos. Animi quibusdam cum omnis officiis
+//                         <br />
+//                         <br />
+//                         voluptatum quo ea eveniet? Lorem ipsum dolor sit amet consectetur
+//                         adipisicing elit. Ducimus quae dolor, optio voluptatibus magnam iure
+//                         esse tempora beatae, a suscipit eos. Animi quibusdam cum omnis
+//                         officiis voluptatum quo ea eveniet?
+//                     </p>
+//                 </div>
+//                 <div className="flex-1 space-y-7.5">
+//                     <h1 className="text-3xl font-semibold">What We Do?</h1>
+//                     <p className="text-lg font-light">
+//                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+//                         quae dolor, optio voluptatibus magnam iure esse tempora beatae, a
+//                         suscipit eos. Animi quibusdam cum omnis officiis voluptatum quo ea
+//                         eveniet? Lorem ipsum dolor sit amet consectetur adipisicing elit. -
+//                         Creative Illustrations
+//                         <br />
+//                         <br /> - Dynamic Websites
+//                         <br />
+//                         <br /> - Fast and Handy
+//                         <br />
+//                         <br /> - Mobile Apps
+//                     </p>
+//                     <Button url="/contact" text="Contact" />
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+//
+// export default About;
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Image from "next/image";
+import "./footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className="h-12 text-sm flex items-center justify-between">
+      <div>©2023 Lamamia. All rights reserved.</div>
+      <div className="flex items-center gap-2.5">
+        <Image
+          src="/1.png"
+          width={15}
+          height={15}
+          className=".icon"
+          alt="Lama Dev Facebook Account"
+        />
+        <Image
+          src="/2.png"
+          width={15}
+          height={15}
+          className=".icon"
+          alt="Lama Dev"
+        />
+        <Image
+          src="/3.png"
+          width={15}
+          height={15}
+          className=".icon"
+          alt="Lama Dev"
+        />
+        <Image
+          src="/4.png"
+          width={15}
+          height={15}
+          className=".icon"
+          alt="Lama Dev"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
