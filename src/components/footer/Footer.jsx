61e3c52a2729a8890afffe5e2ba54db96da7acc3
sudami <sudami125@gmail.com>
2023-11-08 16:46:56 +0900

    fix(footer components): 修复tailwindcss 导入失败bug

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import "./footer.module.css";
+// import "./footer.module.css";
 
 const Footer = () => {
   return (
@@ -11,28 +11,28 @@ const Footer = () => {
           src="/1.png"
           width={15}
           height={15}
-          className=".icon"
+          className="icon"
           alt="Lama Dev Facebook Account"
         />
         <Image
           src="/2.png"
           width={15}
           height={15}
-          className=".icon"
+          className="icon"
           alt="Lama Dev"
         />
         <Image
           src="/3.png"
           width={15}
           height={15}
-          className=".icon"
+          className="icon"
           alt="Lama Dev"
         />
         <Image
           src="/4.png"
           width={15}
           height={15}
-          className=".icon"
+          className="icon"
           alt="Lama Dev"
         />
       </div>
