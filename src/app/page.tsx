02e0f99b634be77354df4545fe255b3a616b7313
sudami <sudami125@gmail.com>
2023-12-31 14:50:25 +0900

    feat(package, blog, not-found): up to next 14, add bog and not found page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,28 @@
-import Image from 'next/image'
-
+import Image from "next/image";
+import Hero from "../../public/hero.png";
 export default function Home() {
   return (
-      <div className="flex items-center space-x-24">
-          <div className="flex-1 flex flex-col space-y-12">
-              <h1 className="text-7xl font-bold"style={{
-                  background: 'linear-gradient(to bottom, #194c33, #bbb)',
-                  WebkitBackgroundClip: 'text',
-                  WebkitTextFillColor: 'transparent'
-              }}>
-                  Better design for your digital products.
-              </h1>
-              <p className='text-xl font-light'>
-                  Turning your Idea into Reality. We bring together the teams from the
-                  global tech industry.
-              </p>
-              {/*<Button url="/portfolio" text="See Our Works"/>*/}
-          </div>
-          <div className='flex-1 flex flex-col space-y-12'>
-              {/*<Image src={hero} alt="" className={styles.img} />*/}
-          </div>
+    <div className="flex items-center space-x-24 ">
+      <div className="flex-1 flex flex-col space-y-12">
+        <h1
+          className="text-7xl font-bold leading-tight"
+          style={{
+            background: "linear-gradient(to bottom, #194c33, #bbb)",
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
+          }}
+        >
+          Better design for your digital products.
+        </h1>
+        <p className="text-xl font-light">
+          Turning your Idea into Reality. We bring together the teams from the
+          global tech industry.
+        </p>
+        {/*<Button url="/portfolio" text="See Our Works"/>*/}
       </div>
-  )
+      <div className="flex-1 flex flex-col space-y-12">
+        <Image src={Hero} alt="" className="w-full h-auto object-contain" />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./button.module.css";
+import Link from "next/link";
+
+const Button = ({ text, url }: { text: string; url: string }) => {
+  return (
+    <Link href={url}>
+      <button className="p-5 cursor-pointer bg-[#53c28b] border-none rounded-md max-w-max text-white">
+        {text}
+      </button>
+    </Link>
+  );
+};
+
+export default Button;
